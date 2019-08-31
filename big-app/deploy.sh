0f152d529c7db7d2096656b315d70867d9cf3d06
Neil Peterson <neilpeterson@outlook.com>
2019-08-31 10:48:40 -0700

    TS

--- big-app/deploy.sh
+++ big-app/deploy.sh
@@ -91,9 +91,15 @@ git clone https://github.com/Azure-Samples/ignite-tour-lp1s1.git
 printf "\n*** Deploying the App Services and Cosmos DB ***\n"
 
 az group deployment create -g $RESOURCE_GROUP_NAME --template-file appservicedeploy.json --parameters prefix=$RESOURCE_PREFIX location=$LOCATION sqlVMIPAddress=$SQL2012_VM_IP_ADDRESS sqlAdminLogin=$USERNAME sqlAdminPassword=$PASSWORD
+echo "******sleep*******"
+echo "******sleep*******"
+echo "******sleep*******"
+echo "******sleep*******"
+
+sleep 20m
 
 # *** DELETE EVERYTHING TO THE NEXT 3 asterisks
-#cd ignite-tour-lp1s1/deployment
+cd ignite-tour-lp1s1/deployment
 # *** DONE DELETING
 
 # *** UNDELETE THE NEXT LINE
